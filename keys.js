232a18d698fa13dcad420312baf28ec71df218fa
Faisal N <narsi.faisal@gmail.com>
2017-01-22 13:23:29 -0600

    node app liri

---
+++ keys.js
@@ -0,0 +1,8 @@
+	console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'h8gPLJu6SZJmtDqb8AfCMBRtK',
+  consumer_secret: '7krmYAA05wmunj7mdnwkXE5J0OUiOBg0JNXEKJ6fOSQS2Fwtt4',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
